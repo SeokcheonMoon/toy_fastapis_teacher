043ff8188cbca6544511e01bd7b3a95645943753
seokcheon <my980519@gmail.com>
2024-01-12 09:58:25 +0900

    something

--- main.py
+++ main.py
@@ -33,6 +33,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from fastapi.staticfiles import StaticFiles                                                   # http://127.0.0.1:8000/resources/css/commons.css가 실행되도록 하는 구문
+# url 경로 자원 물리경로 프로그래밍 측면
+app.mount("/css", StaticFiles(directory="resources\\css\\"), name="static_css")               # http://127.0.0.1:8000/resources/css/commons.css가 실행되도록 하는 구문
+app.mount("/images", StaticFiles(directory="resources\\images\\"), name="static_images") 
+# /css는 url의 경로 
+# directory 자리에는 resources의 css의 Copy relative Path를 넣어준다.  
+# name 의 자리는 그냥 유니크한 이름을 정하면 된다.(중복x)
+
 @app.get("/")
 async def root(Request:Request):
     # return {"message": "jisu World"}
