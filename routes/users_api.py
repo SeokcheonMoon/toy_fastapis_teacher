492881a772bdfd8169db57e151080823ca9a2b4e
seokcheon <my980519@gmail.com>
2024-01-22 16:20:16 +0900

    .good

--- routes/users_api.py
+++ routes/users_api.py
@@ -17,36 +17,36 @@ router = APIRouter()
 
 #회원가입
 @router.post("/new")
-async def create_event(body: User) -> dict:
+async def create_users(body: User) -> dict:
     document = await user_database.save(body)
     return {
-        "message": "Event created successfully"
+        "message": "users created successfully"
         ,"datas": document
     }
 
 #로그인
 @router.get("/{id}", response_model=User)
-async def retrieve_event(id: PydanticObjectId) -> User:
+async def retrieve_users(id: PydanticObjectId) -> User:
     users = await user_database.get(id)
     if not users:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Event with supplied ID does not exist"
+            detail="users with supplied ID does not exist"
         )
     return users
 
 #회원탈퇴
 @router.delete("/{id}")                                                                     # function이 delete
-async def delete_event(id: PydanticObjectId) -> dict:
+async def delete_users(id: PydanticObjectId) -> dict:
     users = await user_database.get(id)
     if not users:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Event not found"
+            detail="users not found"
         )
     users = await user_database.delete(id)
 
     return {
-        "message": "Event deleted successfully."
+        "message": "users deleted successfully."
         ,"datas": users
     }
